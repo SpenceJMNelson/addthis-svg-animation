f1929874d2bd092dd2be3234dc56fb60e6da319d
Spence Nelson <spenjmn@me.com>
2016-10-10 15:20:11 -0400

    animation updates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 500) {
+    if (scroll >= 1000) {
         $(".section-2 svg").addClass("active");
     } else {
         $(".section-2 svg").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $(".tool-illustration-wrapper").addClass("active");
+    } else {
+        $(".tool-illustration-wrapper").removeClass("active");
     }
 });
