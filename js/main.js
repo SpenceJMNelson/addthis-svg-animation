3851fe281f79e0ce58d77397e97062be6b6ea265
Spence Nelson <spenjmn@me.com>
2016-10-24 11:25:21 -0400

    Full animation buildout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 1000) {
+    if (scroll >= 900) {
+        $(".tool-illustration-wrapper").addClass("active");
+    } else {
+        $(".tool-illustration-wrapper").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1600) {
         $(".section-2 svg").addClass("active");
     } else {
         $(".section-2 svg").removeClass("active");
@@ -11,9 +21,39 @@ $(window).scroll(function() {
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 500) {
-        $(".tool-illustration-wrapper").addClass("active");
+    if (scroll >= 2300) {
+        $(".section-3 svg").addClass("active");
     } else {
-        $(".tool-illustration-wrapper").removeClass("active");
+        $(".section-3 svg").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 3000) {
+        $(".section-4 svg").addClass("active");
+    } else {
+        $(".section-4 svg").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 3800) {
+        $(".section-5 svg").addClass("active");
+    } else {
+        $(".section-5 svg").removeClass("active");
+    }
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 4500) {
+        $(".section-6 svg").addClass("active");
+    } else {
+        $(".section-6 svg").removeClass("active");
     }
 });
